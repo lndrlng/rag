666ee1a349c40417df9621c8a9816850dfd0a7b5
david <david@glaesle.cloud>
2025-04-03 10:05:55 +0200

    test actions

---
+++ test.md
@@ -0,0 +1,4 @@
+# lets test if the markdown ruleset applies for very very long lines bla bla bla bla bla bla l
+
+# header
+-list
---
+++ test.py
@@ -0,0 +1,13 @@
+import os, sys
+
+
+def test():
+    print("Hello, Linter!")
+    x = 42
+    y = 7
+    if x > y:
+        print("x is greater")
+    return x + y
+
+
+test()
